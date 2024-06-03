7b8bb0d9594e72c75addf4845e58a1f7f0a47a39
pedro roca de togores <pedrobgrdt@gmail.com>
2024-06-04 00:38:38 +0200

    full finish

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      if (printTimeCallback) printTimeCallback()
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    const minutes = Math.floor(this.currentTime / 60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = Math.floor(this.currentTime % 60)
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    return ('0' + value).slice(-2)
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    const seconds = this.computeTwoDigitNumber(this.getSeconds())
+    const minutes = this.computeTwoDigitNumber(this.getMinutes())
+    return `${minutes}:${seconds}`
   }
 }
